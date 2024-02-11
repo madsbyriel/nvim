88894ebc11e72513a24547568f695ff319e3db14
Mads Byriel <madshbyriel@gmail.com>
2024-02-11 16:25:31 +0100

    Keybind changes to LSP.
    Mapped confirm selection to just enter, and now you move up and down using ctrl j/k.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,9 +4,20 @@ lsp_zero.on_attach(function(client, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp_zero.default_keymaps({buffer = bufnr})
+
 end)
 
+local cmp = require('cmp')
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		-- confirm completion
+		['<CR>'] = cmp.mapping.confirm({select = true}),
 
+		-- move up and down
+		['<C-k>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+		['<C-j>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+	})
+})
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
