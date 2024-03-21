239026a83c68058a28e8b66c463dac2787348e67
Mads Byriel <madshbyriel@gmail.com>
2024-03-21 21:43:17 +0100

    Map for quick G and commit
    Created the keymaps leader-g for :G and leader-gc for G commit.

--- lua/madsbyriel/remap.lua
+++ lua/madsbyriel/remap.lua
@@ -1,3 +1,16 @@
 vim.g.mapleader = " "
+
+-- General
+-- Exit and go to directory view
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Insert ';' at the end of the line
 vim.keymap.set("n", "<leader>;", '$a;', { noremap = true })
+
+
+-- Fugitive
+-- Open up the Git window from fugitive
+vim.keymap.set("n", "<leader>g", ":G<CR>", { noremap = true })
+
+-- Start a commit message
+vim.keymap.set("n", "<leader>gc", ":G commit<CR>", { noremap = true })
