4e1ed2a5ab76bc2056ccca18b485d39600dbf5b5
Mads Byriel <madshbyriel@gmail.com>
2024-07-03 14:24:42 +0200

    Added dart support for dev in flutter

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,42 +1,40 @@
 local lsp_zero = require('lsp-zero')
+local lsp_config = require('lspconfig')
+local cmp = require('cmp')
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({buffer = bufnr})
 
 end)
 
-local cmp = require('cmp')
 cmp.setup({
-	mapping = cmp.mapping.preset.insert({
-		-- confirm completion
-		['<CR>'] = cmp.mapping.confirm({select = true}),
+    mapping = cmp.mapping.preset.insert({
+        -- confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
 
-		-- move up and down
-		['<C-k>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-		['<C-j>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-	})
+        -- move up and down
+        ['<C-k>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+        ['<C-j>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    })
 })
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {
+    ensure_installed = {
         "rust_analyzer",
     },
-	handlers = {
-		lsp_zero.default_setup,
-	},
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 
-local lspconfig = require('lspconfig')
-lspconfig.mojo.setup({
-    cmd={'mojo-lsp-server'},
-    filetypes={'mojo'},
-})
-lspconfig.rust_analyzer.setup({})
-lspconfig.eslint.setup({
+lsp_config.rust_analyzer.setup({})
+lsp_config.eslint.setup({
     cmd={'vscode-eslint-language-server'}
 })
+
+lsp_config["dartls"].setup({})
